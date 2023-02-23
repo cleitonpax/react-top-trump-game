b682b941f8d5209dc012b500ab680596b0f1c0ba
Cleiton S <cleiton.pax@gmail.com>
2023-02-23 20:05:47 -0300

    Project init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from "react";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [connected, setConnected] = useState(false);
+  const [message, setMessage] = useState("");
+  const socketRef = useRef(null);
+
+  useEffect(() => {
+    const socket = new WebSocket("ws://localhost:8080");
+
+    socketRef.current = socket;
+
+    socket.onopen = () => {
+      console.log("WebSocket connection established");
+      setConnected(true);
+    };
+
+    socket.onmessage = (event) => {
+      console.log("Received message:", event.data);
+      setMessage(event.data);
+    };
+
+    socket.onerror = (error) => {
+      console.error("WebSocket error:", error);
+    };
+
+    socket.onclose = () => {
+      console.log("WebSocket connection closed");
+      setConnected(false);
+    };
+
+    return () => {
+      socketRef.current.close();
+    };
+  }, []);
+
+  const handleInputChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    if (socketRef.current && connected) {
+      socketRef.current.send(username);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="container rounded p-3 d-flex align-items-center flex-column">
+      <h1 className="text-center mb-5 text-white">React Top Trump</h1>
+      <form
+        onSubmit={handleFormSubmit}
+        className="text-center d-flex flex-column"
+      >
+        <div className="form-group">
+          <input
+            type="text"
+            className="form-control text-center p-3"
+            placeholder="Type your username"
+            value={username}
+            onChange={handleInputChange}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary mt-3 btn-block btn-lg">
+          Enter
+        </button>
+      </form>
+      <footer className="text-center mb-5 text-white fixed-bottom opacity-50">
+        2023 by @cleitonpax and @garusocruz
+      </footer>
+      {connected && (
+        <p className="text-center mt-5 text-success">
+          Connected to server. Last message received: <strong>{message}</strong>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      )}
+      {!connected && (
+        <p className="text-center mt-5 text-warning">Not connected to server</p>
+      )}
     </div>
   );
 }
